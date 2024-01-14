cf7ba560c71cba8fc3d5517625101bef23ef6f6f
zach-withcoherence <86851918+zach-withcoherence@users.noreply.github.com>
2024-01-14 16:35:36 -0500

    WIP

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 import imp
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "sqlite:///./db.sqlite"
-
-engine = create_engine(DB_URL, connect_args = { "check_same_thread": False })
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"].replace('postgres://', 'postgresql://')
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = { "check_same_thread": False })
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
